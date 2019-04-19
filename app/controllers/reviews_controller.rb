35f1681008a3a881d3c275d0a08ae1c65c3dca11
Tess <tess.thornley@gmail.com>
2019-04-19 20:59:20 +0000

    bug: error messages displaying on signup page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
 
-  #before_filter: authorize
+  before_filter: authorize
 
   def create
     @review = Review.new(review_params)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
   
   def new
-    user = User.new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to [:root]
     else
-      redirect_to [:signup]
+      render :new
     end
   end
 
