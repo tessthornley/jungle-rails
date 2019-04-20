2c5cf7737f8646c49c280457c30e04dec755c72e
Tess <tess.thornley@gmail.com>
2019-04-20 16:50:22 +0000

    reviews controller readded

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @review.product_id = params[:product_id]
+    @review.user_id = current_user.id
+    
+
+    if @review.save
+      redirect_to "/products/#{@review.product_id}"
+    else
+      redirect_to Product.find(@review.product_id)
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy!
+    redirect_to Product.find(@review.product_id)
+  end
+
+  #before_filter: authorize
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
+  end
+
+end
