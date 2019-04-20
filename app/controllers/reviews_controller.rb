3edca3d94af795ecab6c654da3452c4fd750fdb9
Tess <tess.thornley@gmail.com>
2019-04-20 16:54:30 +0000

    destroy method updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,7 +18,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find(params[:id])
     @review.destroy!
-    redirect_to Product.find(@review.product_id)
+    redirect_to "/products/#{@review.product_id}"
   end
 
   private
